4a269fc155579aac911b52f1ec0ef186aa2297ce
Irina <109478015+Irina-Levkovich@users.noreply.github.com>
2023-01-17 15:14:57 +0300

    new_project_test_corrected

--- expenses/views.py
+++ expenses/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.list import ListView
+from django.http import HttpResponse
+from django.template import loader
 
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
@@ -24,7 +26,104 @@ class ExpenseListView(ListView):
             summary_per_category=summary_per_category(queryset),
             **kwargs)
 
+
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
+
+#functions for sorting
+class Sort(ListView):
+    model = Expense
+    paginate_by = 5
+
+    def sort_date_to(request):
+        mydata = Expense.objects.all().order_by('-date').values()
+        template = loader.get_template('expense_list.html')
+        context = {
+            'sort': mydata,
+        }
+        return HttpResponse(template.render(context, request))
+
+    def sort_date_from(request):
+        data = Expense.objects.all().order_by('date').values()
+        template = loader.get_template('expense_list.html')
+        context = {
+            'sorting': data,
+            }
+        return HttpResponse(template.render(context, request))
+
+    def sort_name(request):
+        name = Expense.objects.all().order_by('name').values()
+        template = loader.get_template('expense_list.html')
+        context = {
+            'sorted': name,
+            }
+        return HttpResponse(template.render(context, request))
+
+    def sort_amount(request):
+        amo = Expense.objects.all().order_by('amount').values()
+        template = loader.get_template('expense_list.html')
+        context = {
+            'sorter': amo,
+            }
+        return HttpResponse(template.render(context, request))
+
+    def sort_category(request):
+        cat = Expense.objects.all().order_by('category').values()
+        template = loader.get_template('expense_list.html')
+        context = {
+            'sorters': cat,
+            }
+        return HttpResponse(template.render(context, request))
+
+#funtions for searching
+class Search(ListView):
+    model = Expense
+    paginate_by = 5
+
+    def search_date_to(request):
+        mydata = Expense.objects.all().filters('-date').values()
+        template = loader.get_template('expense_list.html')
+        context = {
+            'search': mydata,
+        }
+        return HttpResponse(template.render(context, request))
+
+    def search_date_from(request):
+        data = Expense.objects.all().filters('date').values()
+        template=loader.get_template('expense_list.html')
+        context = {
+                'searching': data,
+        }
+        return HttpResponse(template.render(context, request))
+
+    def search_name(request):
+        name = Expense.objects.all().filters('name').values()
+        template=loader.get_template('expense_list.html')
+        context = {
+                'searcher': name,
+        }
+        return HttpResponse(template.render(context, request))
+
+    def search_amount(request):
+        amount = Expense.objects.all().filters('-date').values()
+        template = loader.get_template('expense_list.html')
+        context = {
+            'searchers': amount,
+        }
+        return HttpResponse(template.render(context, request))
+
+    def search_category(request):
+        category = Expense.objects.all().filters('-date').values()
+        template=loader.get_template('expense_list.html')
+        context = {
+                'searcherss': category,
+        }
+        return HttpResponse(template.render(context, request))
+
+
+
+
+
+
 
